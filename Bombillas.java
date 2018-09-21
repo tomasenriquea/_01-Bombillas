3d826d1f112ff35391d15236166af85b6c3b43c9
Tomas Enrique <tomasenriquea80@gmail.com>
2018-09-21 16:34:05 +0200

    Initial commit

---
+++ Bombillas.java
@@ -0,0 +1,5 @@
+package uF4.trabajos.A1_17_09_2018._01;
+
+public class Bombillas {
+
+}
---
+++ Main.java
@@ -0,0 +1,28 @@
+package uF4.trabajos.A1_17_09_2018._01;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		
+		
+
+	}
+
+}
+
+
+/*
+A12 - Bombillas de intensidad variable
+    Queremos utilizar bombillas de intensidad variable para un videojuego.
+    La intensidad de la bombilla toma un valor de 0 a 5, 0 si la bombilla está apagada. Si la intensidad sobrepasa el nivel 5 la 
+    bombilla se quema y deja de funcionar. Ya no se puede modificar la intensidad de la bombilla si está apagada.
+
+[]1) Diseñar una aplicación Java que controle seis bombillas de manera las lleve a un estado inicial en que una bombilla esté 
+     apagada y cada una de las otras tenga una intensidad diferente. Muestre este estado inicial
+
+[]2) Active la intensidad de cada una de las bombillas y mostrad su estado
+
+[]3) Baje la intensidad de cada una de las bombillas y mostrad su estado
+*/
